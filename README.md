2ed04297992c4a0515eb431c89016a42c361c146
daimian.123456 <daimian.123456@bytedance.com>
2021-10-30 16:59:20 +0800

    feat: update webpack to version 5

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-webpack-template
-A WebPack template written in the React language. It‘s using by my react-cli
+A WebPack template written in the React language. It‘s used by my react-cli
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,23 +1,19 @@
 // config/webpack.config.dev.js
-const { merge } = require("webpack-merge");
-const baseConfig = require("./webpack.common.js");
-const path = require("path");
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(baseConfig, {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 3000,
-    host: "localhost",
-    contentBase: path.join(__dirname, "../src"),
-    watchContentBase: true,
-    publicPath: "/",
+    host: 'localhost',
     compress: true,
-    historyApiFallback: true,
     hot: true,
-    clientLogLevel: "error",
     open: true,
-    watchOptions: {
-      ignored: /node_modules/,
-    },
+    historyApiFallback: true,
+  },
+  watchOptions: {
+    ignored: /node_modules/,
   },
 });
